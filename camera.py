e2dc1f2072e9b8f889f23f702841f0eee58a1343
linky <wladtfmf@gmail.com>
2017-08-14 12:59:46 +0000

    added autostart; refactoring; prints

--- camera.py
+++ camera.py
@@ -2,6 +2,7 @@
 
 import os
 import sh
+import sys
 import glob
 import picamera
 
@@ -22,14 +23,17 @@ def getUsbDrive():
 			path = "/sys/class/block/" + deviceName
 		if os.path.islink(path):
 			if os.path.realpath(path).find("/usb") > 0:
-				return "/dev/" + deviceName + '1'
+				print('/dev/' + deviceName + '1')
+				return '/dev/' + deviceName + '1'
 	return None
 
 def mountDrive():
 	drivePath = getUsbDrive()
+	print('mount %s' % drivePath)
 	sh.mount(drivePath, VIDEO_DIR)
 
 def umountDrive():
+	print('umount %s' % VIDEO_DIR)
 	sh.umount(VIDEO_DIR)
 
 def genNewVideoPath():
@@ -60,14 +64,41 @@ def writeVideo():
 
 	# write 15-minutes parts of stream
 	for filename in camera.record_sequence(['%d.' + FORMAT % (part + 1) for part in range(999)]):
+		print('start writing %s' % filename)
+		# check if avaliable space on sdcard < 80%
+		used_ratio = getDriveUsedRatio()
+		print('sdcard space available: %f%%' % used_ratio)
+		while used_ratio > 0.8: 
+			old_video = getOldVideoPath()
+			os.remove(old_video)
+			used_ratio = getDriveUsedRatio()
+
 		camera.wait_recording(VIDEO_LENGTH*60) # 15 min
 
+def setAutostart():
+	self_path = os.path.realpath(__file__)
+	print('set autostart %s' % self_path)
+	with open('/etc/rc.local') as fin:
+		with open('/etc/rc.local.tmp') as fout:
+			while line in fin:
+				if line == 'exit 0':
+					fout.write(self_path + ' &\n')
+				fout.write(line)
 
-###############################3#
+	os.rename('/etc/rc.local', '/etc/rc.local.old')
+	os.rename('/etc/rc.loca.tmp', '/etc/rc.local')
 
+
+#######################################################################
+
+# autostart
+if len(sys.argv) == 2 and sys.argv[1] == '-a':
+	print('set autostart')
+	setAutostart()
 
 # check sdcard
 while True:
+	print('check sdcard')
 	if not getUsbDrive():
 		os.sleep(3)
 		continue
@@ -75,13 +106,6 @@ while True:
 # processing
 try:
 	mountDrive()
-
-	# check if avaliable space on sdcard < 80%
-	used_ratio = getDriveUsedRatio()
-	while used_ratio > 0.8: 
-		old_video = getOldVideoPath()
-		os.remove(old_video)
-		used_ratio = getDriveUsedRatio()
 
 	writeVideo();
 
